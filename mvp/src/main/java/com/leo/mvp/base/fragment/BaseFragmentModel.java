ffdacb1505cca94b8a826a9a33efdd3552d1646e
Leo <lewen@i-focusing.com>
2017-09-14 15:43:04 +0800

    修改BaseFragmentModel .

--- mvp/src/main/java/com/leo/mvp/base/fragment/BaseFragmentModel.java
+++ mvp/src/main/java/com/leo/mvp/base/fragment/BaseFragmentModel.java
@@ -1,13 +1,10 @@
 package com.leo.mvp.base.fragment;
 
-import javax.inject.Inject;
-
 /**
  * Created by Leo on 2017/7/20.
  */
 
 public abstract class BaseFragmentModel<T extends BaseFragmentPresenter> {
-    @Inject
     protected T mPresenter;
 
     public void setPresenter(T presenter) {
